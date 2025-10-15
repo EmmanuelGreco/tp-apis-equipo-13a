bcf3a9547e5c8f83b0dcc01d3a6f7493cca49325
p14082003 <ptaquino2003@gmail.com>
2025-10-15 01:57:17 -0300

    Validaciones múltiples en todos los metodos. Capacidad de enviar entre 1 y n campos en el PUT, y de crear en POST con solo los id de categoria y marca (queda vacio cod. nombre y desc., y el precio en 0.

--- TPAPIs_equipo-13A/WebAPI/Controllers/ArticuloController.cs
+++ TPAPIs_equipo-13A/WebAPI/Controllers/ArticuloController.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Dominio;
+using Negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Web.Http;
-using Dominio;
-using Negocio;
 using WebAPI.Models;
 
 namespace WebAPI.Controllers
@@ -13,60 +14,178 @@ namespace WebAPI.Controllers
     public class ArticuloController : ApiController
     {
         // GET: api/Articulo
-        public IEnumerable<Articulo> Get()
+        public HttpResponseMessage Get()
         {
-            ArticuloNegocio negocio = new ArticuloNegocio();
-            return negocio.listar();
+            try
+            {
+                ArticuloNegocio negocio = new ArticuloNegocio();
+                IEnumerable<Articulo> lista = negocio.listar();
+                
+                if (lista == null || lista.Count() == 0)
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "No se encontraron artículos.");
+
+                return Request.CreateResponse(HttpStatusCode.OK, lista);
+            }
+            catch (Exception)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Ocurrió un error en el servidor.");
+            }
         }
 
         // GET: api/Articulo/5
-        public Articulo Get(int id)
+        public HttpResponseMessage Get(int id)
         {
-            ArticuloNegocio negocio = new ArticuloNegocio();
-            List<Articulo> lista = negocio.listar();
+            try
+            {
+                ArticuloNegocio negocio = new ArticuloNegocio();
+                List<Articulo> lista = negocio.listar();
 
-            return lista.Find(x=> x.Id == id);
+                if (lista == null || lista.Count() == 0)
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "No se encontraron artículos.");
+                
+                Articulo encontrado = lista.Find(x => x.Id == id);
+                if (encontrado == null)
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "No se encontró un artículo con este id.");
+                else 
+                    return Request.CreateResponse(HttpStatusCode.OK, encontrado);
+            }
+            catch (Exception)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Ocurrió un error en el servidor.");
+            }
         }
 
         // POST: api/Articulo
-        public void Post([FromBody]ArticuloDTO articulo)
+        public HttpResponseMessage Post([FromBody]ArticuloDTO articulo)
         {
-            ArticuloNegocio negocio = new ArticuloNegocio();
-            Articulo nuevo = new Articulo();
+            try
+            {
+                if (!ModelState.IsValid)
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new { Message = "IdCategoria e IdMarca deben ser números enteros. Precio debe ser un número decimal." });
 
-            nuevo.Codigo = articulo.Codigo;
-            nuevo.Nombre = articulo.Nombre;
-            nuevo.Descripcion = articulo.Descripcion;
-            nuevo.Marca = new Marca { Id = articulo.IdMarca };
-            nuevo.Categoria = new Categoria { Id =  articulo.IdCategoria };
-            nuevo.Precio = articulo.Precio;
+                ArticuloNegocio articuloNegocio = new ArticuloNegocio();
 
-            negocio.agregar(nuevo);
+                if(articulo.IdMarca == null|| articulo.IdCategoria == null)
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new { Message = "No se han enviado todos los campos correspondientes." });
+
+
+                MarcaNegocio marcaNegocio = new MarcaNegocio();
+                CategoriaNegocio categoriaNegocio = new CategoriaNegocio();
+                
+                Marca marca = marcaNegocio.listar().Find(x => x.Id == articulo.IdMarca);
+                Categoria categoria = categoriaNegocio.listar().Find(x => x.Id == articulo.IdCategoria);
+
+                if (marca == null)
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new { Message = "La marca no existe." });
+
+                if (categoria == null)
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new { Message = "La categoría no existe." });
+
+
+                if(articulo.Precio < 0)
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new { Message = "El precio debe ser mayor o igual a cero." });
+
+                Articulo nuevo = new Articulo
+                {
+                    Codigo =        articulo.Codigo != null         ? articulo.Codigo : "",
+                    Nombre =        articulo.Nombre != null         ? articulo.Nombre : "",
+                    Descripcion =   articulo.Descripcion != null    ? articulo.Descripcion : "",
+                    Marca =         new Marca { Id = (int)articulo.IdMarca },
+                    Categoria =     new Categoria { Id = (int)articulo.IdCategoria },
+                    Precio =        articulo.Precio != null         ? (decimal)articulo.Precio : 0
+                };
+                
+                articuloNegocio.agregar(nuevo);
+                return Request.CreateResponse(HttpStatusCode.OK, new { Message = "Artículo agregado correctamente." });
+            }
+            catch (Exception)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Ocurrió un error en el servidor.");
+            }
         }
 
         // PUT: api/Articulo/5
-        public void Put(int id, [FromBody] ArticuloDTO articulo)
+        public HttpResponseMessage Put(int id, [FromBody]ArticuloDTO articulo)
         {
-            ArticuloNegocio negocio = new ArticuloNegocio();
-            Articulo existente = new Articulo();
+            try
+            {
+                if (!ModelState.IsValid)
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new { Message = "IdCategoria e IdMarca deben ser números enteros. Precio debe ser un número decimal." });
 
-            existente.Id = id;
-            existente.Codigo = articulo.Codigo;
-            existente.Nombre = articulo.Nombre;
-            existente.Descripcion = articulo.Descripcion;
-            existente.Marca = new Marca { Id = articulo.IdMarca };
-            existente.Categoria = new Categoria { Id = articulo.IdCategoria };
-            existente.Precio = articulo.Precio;
+                if (articulo.Codigo == null && articulo.Nombre == null && articulo.Descripcion == null && 
+                   articulo.IdMarca == null && articulo.IdCategoria == null && articulo.Precio == null)
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new { Message = "Ningún campo a modificar." });
 
-            negocio.modificar(existente);
+                ArticuloNegocio articuloNegocio = new ArticuloNegocio();
+                MarcaNegocio marcaNegocio = new MarcaNegocio();
+                CategoriaNegocio categoriaNegocio = new CategoriaNegocio();
+            
+                List<Articulo> lista = articuloNegocio.listar();
+
+                if (lista == null || lista.Count() == 0)
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "No se encontraron artículos.");
+
+                Articulo encontrado = lista.Find(x => x.Id == id);
+                if (encontrado == null)
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "No se encontró un artículo con este id.");
+
+
+                Marca marca = marcaNegocio.listar().Find(x => x.Id == articulo.IdMarca);
+                Categoria categoria = categoriaNegocio.listar().Find(x => x.Id == articulo.IdCategoria);
+                if (articulo.IdMarca != null && marca == null)
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new { Message = "La marca no existe." });
+
+                if (articulo.IdCategoria != null && categoria == null)
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new { Message = "La categoría no existe." });
+
+                if (articulo.Precio < 0)
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new { Message = "El precio debe ser mayor o igual a cero." });
+
+                //if ()
+
+                Articulo modificado = new Articulo()
+                {
+                    Id = id,
+                    Codigo =        articulo.Codigo != null         ? articulo.Codigo                                   : encontrado.Codigo,
+                    Nombre =        articulo.Nombre != null         ? articulo.Nombre                                   : encontrado.Nombre,
+                    Descripcion =   articulo.Descripcion != null    ? articulo.Descripcion                              : encontrado.Descripcion,
+                    Marca =         articulo.IdMarca != null        ? new Marca { Id = (int)articulo.IdMarca }          : encontrado.Marca,
+                    Categoria =     articulo.IdCategoria != null    ? new Categoria { Id = (int)articulo.IdCategoria }  : encontrado.Categoria,
+                    Precio =        articulo.Precio != null         ? (decimal)articulo.Precio                          : encontrado.Precio
+                };
+
+                articuloNegocio.modificar(modificado);
+                return Request.CreateResponse(HttpStatusCode.OK, new { Message = "Artículo modificado correctamente." });
+            }
+            catch (Exception)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Ocurrió un error en el servidor.");
+            }
         }
 
         // DELETE: api/Articulo/5
-        public void Delete(int id)
+        public HttpResponseMessage Delete(int id)
         {
-            ArticuloNegocio negocio = new ArticuloNegocio();
+            try
+            {
+                ArticuloNegocio articuloNegocio = new ArticuloNegocio();
+                List<Articulo> lista = articuloNegocio.listar();
+            
+                Articulo encontrado = lista.Find(x => x.Id == id);
+                if (encontrado == null)
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "No se encontró un artículo con este id.");
+                else
+                {
+                    articuloNegocio.eliminar(id);
+                    return Request.CreateResponse(HttpStatusCode.OK, new { Message = "Artículo eliminado correctamente." });
+                }
+            }
+            catch (Exception)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Ocurrió un error en el servidor.");
+            }
 
-            negocio.eliminar(id);
+                
         }
     }
 }
